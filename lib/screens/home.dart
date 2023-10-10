a6b12ad2e2cc38960cb4a5bd07ff803c52b5275e
Euclydes Uchoas dos Santos Neto <euclydes.uchoas@brgaap.com>
2023-10-10 17:56:31 -0300

    Ajustes na lógica de logout.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:crud_app/auxiliary/constants.dart';
+import 'package:crud_app/models/security/session.dart';
 import 'package:crud_app/screens/annotations/annotation_list.dart';
 import 'package:crud_app/screens/users/user_list.dart';
 import 'package:crud_app/widgets/my_scaffold.dart';
@@ -27,49 +28,93 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return MyScaffold(
-      title: "CRUD App - Home Page",
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(items.length, (index) {
-          final item = items[index];
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: const BoxDecoration(
-                border: Border.fromBorderSide(
-                  BorderSide(width: 2),
+    return WillPopScope(
+      onWillPop: () async {
+        if (!Session.isValid()) return true;
+
+        await confirmLogout();
+        return !Session.isValid();
+      },
+      child: MyScaffold(
+        title: "CRUD App - Home Page",
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(items.length, (index) {
+            final item = items[index];
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: const BoxDecoration(
+                  border: Border.fromBorderSide(
+                    BorderSide(width: 2),
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(45),
+                  ),
+                  color: Colors.green,
                 ),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(45),
+                child: ElevatedButton(
+                  onPressed: () => navigateToPage(context, item["page"]),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size.infinite,
+                    elevation: 0.0,
+                    backgroundColor: Colors.transparent,
+                    disabledBackgroundColor: Colors.transparent,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(item["icon"]),
+                      Text(
+                        item["name"],
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                    ],
+                  ),
                 ),
-                color: Colors.green,
               ),
-              child: ElevatedButton(
-                onPressed: () => navigateToPage(context, item["page"]),
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size.infinite,
-                  elevation: 0.0,
-                  backgroundColor: Colors.transparent,
-                  disabledBackgroundColor: Colors.transparent,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(item["icon"]),
-                    Text(
-                      item["name"],
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
-        }),
+            );
+          }),
+        ),
       ),
     );
+  }
+
+  Future<void> confirmLogout() async {
+    await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Warning"),
+          content: const Text("Are you sure want to logout?"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                /*final response = await AnnotationService.delete(annotationId);
+                if (response.success) {
+                  showSuccessMessage(response.message);
+                  fetchAnnotations();
+                } else {
+                  showErrorMessage(response.message);
+                }*/
+                Session.destroySession();
+                Navigator.pop(context);
+              },
+              child: const Text("Logout"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Cancel"),
+            ),
+          ],
+        );
+      },
+    );
+    return;
   }
 
   void navigateToPage(context, page) {
